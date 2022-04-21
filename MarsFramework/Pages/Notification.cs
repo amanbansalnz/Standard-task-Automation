b0939194b4dd507489ca523a5e3da0b695852ec0
Sreena Narikot <sreesat25@yahoo.co.in>
2022-04-21 12:30:23 +0800

    Notifications

---
+++ MarsFramework/Pages/Notification.cs
@@ -0,0 +1,37 @@
+﻿using MarsFramework.Global;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MarsFramework.Pages
+{
+    internal class Notification
+    {
+        public Notification()
+        {
+            PageFactory.InitElements(GlobalDefinitions.driver, this);
+        }
+        
+        [FindsBy(How = How.XPath, Using = "//*[@id='account-profile-section']/div/div[1]/div[2]/div/div")]
+        private IWebElement clickNotification { get; set; }
+
+        [FindsBy(How = How.XPath, Using = " //*[@id='account-profile-section']/div/div[1]/div[2]/div/div/div/span/div")]
+        private IWebElement notoficationmessage { get; set; }
+       
+        public string notifications()
+        {
+            //GlobalDefinitions.wait(3);
+            Thread.Sleep(2000);
+            clickNotification.Click();
+            Thread.Sleep(2000);
+            string message = notoficationmessage.Text;
+            return message;
+        }
+
+    }
+}
---
+++ MarsFramework/Test/NotificationTest.cs
@@ -0,0 +1,32 @@
+﻿using MarsFramework.Pages;
+using NUnit.Framework;
+using RelevantCodes.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsFramework.Test
+{
+    internal class NotificationTest
+    {
+        [TestFixture]
+        //[Category("Sprint2")]
+        class User : Global.Base
+        {
+            [Test, Order(1)]
+            public void editProfiledetails()
+            {
+                //Testing for no notification message 
+                test = extent.StartTest(" Notification ", "testing Notoficatios ");
+                Notification notificationObj = new Notification();
+                string message = notificationObj.notifications();
+                string expectedMessage = "You have no notifications";
+                Assert.AreEqual(expectedMessage, message);
+                test.Log(LogStatus.Pass, "Test Passed, You have no notifications!");
+
+            }
+        }
+    }
+}
